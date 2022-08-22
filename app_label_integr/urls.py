19a34d59fe0b5a39ad605f8696af32eed939cce0
LavaEKB <lavaekb3@gmail.com>
2022-08-22 17:54:04 +0500

    v1.6

--- app_label_integr/urls.py
+++ app_label_integr/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import MainView
+from .views import MainView, CheckConnectionView, BufferFoodView
 
 urlpatterns = [
     path('', MainView.as_view(), name='index'),
+    path('app_label_integr/iiko/', CheckConnectionView.as_view(), name='checkconnection'),
+    path('app_label_integr/bufferfood/', BufferFoodView.as_view(), name='bufferfood'),
 ]
 
  
--- app_label_integr/views.py
+++ app_label_integr/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from re import T
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from .models import ConnectSettings
+import requests
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
@@ -7,4 +10,54 @@ class MainView(View):
             request,
             'app_label_integr/index.html'
         )
-     
+
+class CheckConnectionView(View):
+    def get(self, request, *args, **kwargs):
+        post = get_object_or_404(ConnectSettings, app_connections='Iiko')
+
+        err = 0 # Флаг ошибки
+
+        # Получаем токен
+        params_login = {
+            "user_id": post.user_id,
+            "user_secret": post.user_secret
+            }
+        try:
+            token = requests.get(post.url_token, params=params_login)
+        except ConnectionError:
+            print("ConnectionError")
+            err = 1
+
+        if err == 0 and token.status_code == 200:
+            token_json = token.json()
+
+        with open('t_file.txt', 'w', encoding='utf-8') as t_f:
+            print(token_json, file=t_f) 
+
+        return render(request, 'app_label_integr/checkconnection.html', context={
+            'post': token_json
+    })     
+
+class BufferFoodView(View):
+    def get(self, request, *args, **kwargs):
+        with open('t_file.txt', encoding='utf-8') as t_f:
+             token = t_f.read().strip()
+        
+        # Список организаций
+        params_orgs = {"access_token": token} 
+        orgs = requests.get("https://iiko.biz:9900/api/0/organization/list", params_orgs)
+        orgs_json = orgs.json()
+
+        # Список блюд первой организации
+        params_prods = {
+            "access_token": token,
+            "revision": "0"
+            } 
+        prods = requests.get(f'https://iiko.biz:9900/api/0/nomenclature/{orgs_json[0]["id"]}', params_prods)
+        prods_json = prods.json()
+
+        #print(prods_json)
+
+        return render(request, 'app_label_integr/bufferfood.html', context={
+            'post': prods_json['groups'][1]
+    })
